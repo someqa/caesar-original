65ce121082ab6eb4a1a80664ebcf5d7ba4363440
unknown <olga.qa@yahoo.com>
2021-05-09 13:12:29 +0300

    feat: add error handling, move processInput to separate file

--- Task-1-Caesar/src/index.js
+++ Task-1-Caesar/src/index.js
@@ -1,21 +1,3 @@
-const inputFile='./input.txt';
-const outputFile='./output.txt'
+const {processInput} = require("./processInput")
 
-const {getShiftedString}=require('./shiftedString');
-const fs=require('fs');
-const { pipeline, Transform } = require('stream');
-
-function processFileInput(inputFilePath, shift, outputFilePath){
-    const transformer=new Transform({
-        transform:(chunk, _, done)=>
-        done(null, getShiftedString(chunk.toString(), shift) )
-    })
-    return pipeline(
-        fs.createReadStream(inputFilePath),
-        transformer,
-        fs.createWriteStream(outputFilePath),
-        (err)=>console.log(err)
-    )
-}
-
-processFileInput(inputFile, 1, outputFile)
+processInput('input.txt', 1, 'output.txt')

7437016ae47524d08efa16557d14fb39ff9a77c2
unknown <olga.qa@yahoo.com>
2021-05-09 14:04:39 +0300

    feat: add handling params with commander

--- Task-1-Caesar/src/index.js
+++ Task-1-Caesar/src/index.js
@@ -1,3 +1,18 @@
-const {processInput} = require("./processInput")
+const { program } = require('commander');
+const { processInput } = require('./processInput');
 
-processInput('input.txt', 1, 'output.txt')
+program
+  .requiredOption('-s, --shift <shift>', 'a shift')
+  .requiredOption('-a, --action <action>', 'an action (encode  or decode)')
+  .option('-i, --input <input>', 'path to input file')
+  .option('-o, --output <output>', 'path to outpu file');
+
+
+program.parse(process.argv);
+const { shift, input, output, action } = program.opts();
+if (action!='encode'&&action!='decode') return console.log('error: unknown action passed, please, pass "encode" or "decode" to -a, --action param');
+
+const shiftNumber = parseInt(shift);
+const adjustedShiftNumber = action == 'encode' ? shiftNumber : -shiftNumber;
+
+processInput(input, adjustedShiftNumber, output);
